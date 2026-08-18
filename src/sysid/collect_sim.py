1fec97b1a4b3f6e39ac59a1fe0100f670a446d2f
Alex Athorne <a.thornysort@gmail.com>
2026-08-18 21:36:50 +0100

    Add step action rollouts

--- src/sysid/collect_sim.py
+++ src/sysid/collect_sim.py
@@ -10,6 +10,9 @@ from tqdm import tqdm
 # params = {"kp": 49.80626029233553, "kv": 2.5679157997056907, "damping": 0.1587636966237435, "frictionloss": 0.006426057652042094, "armature": 0.012995945315907826, "force_limit": 2.4377543428059765}
 # params = {"kp": 77.99970388177452, "kv": 4.021515986037546, "damping": 0.2486338313927485, "frictionloss": 0.010063606282514398, "armature": 0.020352459316455393, "force_limit": 3.817675042441136}
 params = {"kp": 36.81949096939071, "kv": 1.8983427392955468, "damping": 0.11736674184720024, "frictionloss": 0.004750490606992202, "armature": 0.00960730816546369, "force_limit": 2.4}
+params = {'kp': 42.17808137088663, 'kv': 2.1746214415186613, 'damping': 0.13444792031433667, 'frictionloss': 0.005441861744908997, 'armature': 0.011005524924149064, 'force_limit': 2.7492882879418965}
+
+
 if __name__ == '__main__':
     ds = DSInterface('actions')
     env = Env(params=params, action_delay=2)
